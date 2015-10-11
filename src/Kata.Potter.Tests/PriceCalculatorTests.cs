238e5bdd9fbd80ccf8c1c80ea364eb46ccba6558
paul-k <paulk85@outlook.com>
2015-10-11 23:48:22 +0100

    one book added

---
+++ src/Kata.Potter.Tests/PriceCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Kata.Potter.Tests
+{
+    class PriceCalculatorTests
+    {
+        private PriceCalculator _priceCalculator;
+
+        [SetUp]
+        public void Initialise()
+        {
+            _priceCalculator = new PriceCalculator();
+        }
+
+        [Test]
+        public void WhenOneBookIsAdded_ThenReturnsATotalOf8()
+        {
+            _priceCalculator.AddBook("A");
+
+            Assert.That(_priceCalculator.Total, Is.EqualTo(8));
+        }
+    }
+}
---
+++ src/Kata.Potter/PriceCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Kata.Potter
+{
+    public class PriceCalculator
+    {
+        public List<string> Books { get; set; }
+        public int Total
+        {
+            get { return Books.Count * 8; }
+        }
+
+        public PriceCalculator()
+        {
+            Books = new List<string>();
+        }
+
+        public void AddBook(string id)
+        {
+            Books.Add(id);
+        }
+    }
+}
