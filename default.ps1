3f39a323398fcb794c2e308c89a4dff10a968c24
paul-k <paulk85@outlook.com>
2015-11-24 22:16:34 +0000

    cmd line build and test implementation

---
+++ default.ps1
@@ -0,0 +1,13 @@
+Task default -Depends Compile, Test
+
+task Compile {
+    msbuild kata-potter.sln /t:Rebuild /p:Configuration=Release /verbosity:quiet 
+
+    if($lastexitcode -ne 0) {
+      throw "Rebuild Failed!"
+    }  
+}
+
+Task Test -Depends Compile {
+   & tools\NUnit\bin\nunit3-console.exe src\Kata.Potter.Tests\bin\Debug\Kata.Potter.Tests.dll
+}
--- src/Kata.Potter.Tests/PriceCalculatorTests.cs
+++ src/Kata.Potter.Tests/PriceCalculatorTests.cs
@@ -4,20 +4,25 @@ namespace Kata.Potter.Tests
 {
     class PriceCalculatorTests
     {
-        private PriceCalculator _priceCalculator;
-
-        [SetUp]
-        public void Initialise()
-        {
-            _priceCalculator = new PriceCalculator();
-        }
-
         [Test]
         public void WhenOneBookIsAdded_ThenReturnsATotalOf8()
         {
-            _priceCalculator.AddBook("A");
+            var priceCalculator = new PriceCalculator();
 
-            Assert.That(_priceCalculator.Total, Is.EqualTo(8));
+            priceCalculator.AddBook("A");
+
+            Assert.That(priceCalculator.Total, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void WhenTwoDifferentBooksAreAdded_ThenReturnsATotalOf16()
+        {
+            var priceCalculator = new PriceCalculator();
+
+            priceCalculator.AddBook("A");
+            priceCalculator.AddBook("B");
+
+            Assert.That(priceCalculator.Total, Is.EqualTo(16));
         }
     }
 }
